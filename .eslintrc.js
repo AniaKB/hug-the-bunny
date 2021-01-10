07fb1b3d23ace8082dbefd267991fddcc9cb4afc
Gazarino <aniakbennett@gmail.com>
2021-01-10 14:03:17 -0500

    fixing using lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,23 +12,5 @@ module.exports = {
   },
   'plugins': [
     'html'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
+  ]
 };
